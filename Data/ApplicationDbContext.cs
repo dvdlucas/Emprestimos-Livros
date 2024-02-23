e6428047985b0faf972872417f59fe4390110380
David Lucas <davidlucaskl@gmail.com>
2024-02-23 15:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Emprestimos_Livros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Emprestimos_Livros.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<EmprestimosModelcs> Emprestimos { get; set;}
+    }
+}
---
+++ Models/EmprestimosModelcs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Emprestimos_Livros.Models
+{
+    public class EmprestimosModelcs
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
