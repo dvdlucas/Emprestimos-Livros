88cf6014d07b5801ab4024254ed0f8bc02492a50
David Lucas <davidlucaskl@gmail.com>
2024-03-06 14:00:20 -0300

    DELETE AND EDIT IS OK

--- Controllers/EmprestimosController.cs
+++ Controllers/EmprestimosController.cs
@@ -1,6 +1,7 @@
 ﻿using Emprestimos_Livros.Data;
 using Emprestimos_Livros.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Emprestimos_Livros.Controllers
 {
@@ -18,6 +19,7 @@ namespace Emprestimos_Livros.Controllers
             return View(emprestimos);
         }
 
+        [HttpGet]
         public IActionResult Cadastrar()
         {
             return View();
@@ -37,5 +39,74 @@ namespace Emprestimos_Livros.Controllers
             return View();
 
         }
+
+        
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModelcs emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EmprestimosModelcs emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Update(emprestimo);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(emprestimo);
+
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModelcs emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModelcs emprestimo)
+        {
+           if(emprestimo == null)
+            {
+                return NotFound();
+            }
+                _context.Emprestimos.Remove(emprestimo);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            
+        }
+  
     }
 }
