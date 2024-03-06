6f303dc5397b11c07d57df6470fb59e040217f6c
David Lucas <davidlucaskl@gmail.com>
2024-03-06 09:38:44 -0300

    Cadastro de empretimo funcionando

---
+++ Controllers/EmprestimosController.cs
@@ -0,0 +1,41 @@
+﻿using Emprestimos_Livros.Data;
+using Emprestimos_Livros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Emprestimos_Livros.Controllers
+{
+    public class EmprestimosController : Controller
+    {
+        readonly private ApplicationDbContext _context;
+        public EmprestimosController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModelcs> emprestimos = _context.Emprestimos;
+            return View(emprestimos);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModelcs emprestimo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Emprestimos.Add(emprestimo);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+    }
+}
